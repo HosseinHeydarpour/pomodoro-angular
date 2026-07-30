3bfc090b8862087301e0636429453c9ea0b795d5
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2026-07-30 11:02:16 +0330

    feat: add header section

--- src/app/features/dashborad/components/timer/timer.ts
+++ src/app/features/dashborad/components/timer/timer.ts
@@ -6,6 +6,4 @@ import { Component } from '@angular/core';
   templateUrl: './timer.html',
   styleUrl: './timer.scss',
 })
-export class Timer {
-
-}
+export class Timer {}
--- src/app/layouts/main-layout/main-layout/main-layout.ts
+++ src/app/layouts/main-layout/main-layout/main-layout.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Sidebar } from '../../components/sidebar/sidebar';
 import { Footer } from '../../components/footer/footer';
+import { Header } from '../../components/header/header';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [RouterOutlet, Sidebar, Footer],
+  imports: [RouterOutlet, Sidebar, Footer, Header],
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.scss',
 })
