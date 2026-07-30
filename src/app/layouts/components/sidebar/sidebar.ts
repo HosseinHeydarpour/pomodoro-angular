c26271060b603527c19106a95a6cd195f30bfb0f
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2026-07-30 10:34:06 +0330

    feat: add footer and sidebar for the app

--- src/app/layouts/components/sidebar/sidebar.ts
+++ src/app/layouts/components/sidebar/sidebar.ts
@@ -1,11 +1,76 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+interface NavItem {
+  label: string;
+  route: string;
+  iconPath: string;
+}
+
+interface StreakBar {
+  day: string;
+  height: number;
+  active: boolean;
+}
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.scss',
 })
 export class Sidebar {
+  readonly navItems: NavItem[] = [
+    {
+      label: 'Timer',
+      route: '/',
+      iconPath: 'M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z',
+    },
+    {
+      label: 'Tasks',
+      route: '/tasks',
+      iconPath:
+        'M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01',
+    },
+    {
+      label: 'Statistics',
+      route: '/statistics',
+      iconPath:
+        'M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z',
+    },
+    {
+      label: 'Calendar',
+      route: '/calendar',
+      iconPath:
+        'M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z',
+    },
+    {
+      label: 'Focus Music',
+      route: '/music',
+      iconPath:
+        'M9 19V6l12-3v13M9 19c0 1.105-1.343 2-3 2s-3-.895-3-2 1.343-2 3-2 3 .895 3 2zm12 0c0 1.105-1.343 2-3 2s-3-.895-3-2 1.343-2 3-2 3 .895 3 2zM9 10l12-3',
+    },
+    {
+      label: 'Goals',
+      route: '/goals',
+      iconPath: 'M13 10V3L4 14h7v7l9-11h-7z',
+    },
+    {
+      label: 'Settings',
+      route: '/settings',
+      iconPath:
+        'M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z',
+    },
+  ];
 
+  readonly streakBars: StreakBar[] = [
+    { day: 'M', height: 40, active: true },
+    { day: 'T', height: 75, active: true },
+    { day: 'W', height: 50, active: true },
+    { day: 'T', height: 90, active: true },
+    { day: 'F', height: 60, active: true },
+    { day: 'S', height: 100, active: true },
+    { day: 'S', height: 35, active: false },
+  ];
 }
